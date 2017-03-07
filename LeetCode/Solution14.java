b34fb3b152c2943d26cd5f17b17a54e9915020c7
LuluffyLee <494211175@qq.com>
2017-03-07 15:04:43 +0800

    add solution14

---
+++ LeetCode/Solution14.java
@@ -0,0 +1,31 @@
+//LeetCode
+//20. Valid Parentheses
+import java.util.*;
+
+public class Solution14{
+	//Sublime Run
+	public static void main(String[] args)
+	{
+		String[] strs={"1234gsdgrewget34","uasdnauis","12345eawd"};
+		System.out.println(longestCommonPrefix(strs));
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+		int maxIndex=0;
+		String longestCommonStr="";
+		if(strs.length==0)
+			{
+				return longestCommonStr;
+			}
+        for(int k=0;k<strs[0].length();k++) {
+        	int i=0;
+        	for(;i<strs.length&&strs[i].length()>k;i++)
+        	{
+        		if(strs[i].charAt(k)!=strs[0].charAt(k))
+        			return longestCommonStr;
+        	}
+        	if(i==strs.length){longestCommonStr=strs[0].substring(0,k+1);}
+        }
+        return longestCommonStr;
+    }
+}
